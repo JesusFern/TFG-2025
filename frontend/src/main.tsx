f3a9a7563401e92b58d3a0c0511ef4a93b7abf90
Miguel González Ortiz <miggonort1@alum.us.es>
2025-06-19 21:49:34 +0200

    fix: Arreglos en la pagina principal

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { MantineProvider } from '@mantine/core';
+import '@mantine/core/styles.css';
 import "./styles/index.css";
 import AppRoutes from "./routes/routes";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+  <MantineProvider
+    theme={{
+      /* otros ajustes */
+    }}
+    defaultColorScheme="dark"
+  >
     <BrowserRouter>
       <AppRoutes />
     </BrowserRouter>
+  </MantineProvider>
   </React.StrictMode>
 );
