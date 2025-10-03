cc49a724b1dce5c53a866f237711efef6a0637cd
miggonort1 <migonor1@gmail.com>
2025-10-03 12:31:32 +0200

    fix: Errores de workflows

--- backend/jest.config.ts
+++ backend/jest.config.ts
@@ -6,6 +6,11 @@ module.exports = {
     '^.+\\.tsx?$': 'ts-jest'
   },
   setupFilesAfterEnv: ['./tests/setup.ts'],
-  maxWorkers: 2,
+  maxWorkers: 1,
   workerIdleMemoryLimit: '512MB',
+  testTimeout: 30000,
+  // Limpiar mocks y caché entre tests
+  clearMocks: true,
+  resetMocks: true,
+  restoreMocks: true,
 };
